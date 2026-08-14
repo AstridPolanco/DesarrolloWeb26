938e07324e0a8ed7e4b49f75043aa40c87e7a9e4
Astrid Polanco <apenap1@miumg.edu.gt>
2026-08-14 10:21:06 -0600

    feat: add toggle and filters

--- Sesion-04-JavaScript-Parte-1/src/app.js
+++ Sesion-04-JavaScript-Parte-1/src/app.js
@@ -67,8 +67,14 @@ export function eliminarTarea(id) {
  * @returns {boolean}
  */
 export function toggleTarea(id) {
-    // TODO: recorrer `tareas` y cambiar `completada` de la que coincida.
-    // Devuelve true si la encontró.
+    const tarea = tareas.find((t) => t.id === id);
+
+    if (!tarea) {
+        return false;
+    }
+
+    tarea.completada = !tarea.completada;
+    return true;
 }
 
 /**
@@ -77,7 +83,13 @@ export function toggleTarea(id) {
  * @returns {Array}
  */
 export function filtrarTareas(filtro) {
-    // TODO: implementar la lógica de filtrado.
+    if (filtro === "pendientes") {
+        return tareas.filter((tarea) => !tarea.completada);
+    }
+    if (filtro === "completadas") {
+        return tareas.filter((tarea) => tarea.completada);
+    }
+    return tareas;
 }
 
 /**
